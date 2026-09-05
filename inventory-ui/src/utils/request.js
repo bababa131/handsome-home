4349a673b7baa4c81088d848a66bbc1aebc4bf9a
shuaige <1393741776@qq.com>
2026-09-05 10:13:14 +0800

    库存

--- inventory-ui/src/utils/request.js
+++ inventory-ui/src/utils/request.js
@@ -3,7 +3,7 @@ import { ElMessage } from 'element-plus'
 import router from '../router'
 
 const request = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: '',
     timeout: 10000
 })
 
